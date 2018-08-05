7f308ec4ec71300e528bc583263b236a69920ce1
Noi Skuberg <kanokpit.skuberg@gmail.com>
2018-08-05 21:02:02 +0700

    Update content.

---
+++ crud/index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>PHP Easy CRUD</title>
+  <link href="public/css/bootstrap.min.css" rel="stylesheet">
+  <link href="public/css/custom.css" rel="stylesheet">
+</head>
+
+<body>
+  <section class="first-blood">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 content">
+          <h4>ทำ CRUD ง่ายๆ สไตล์ PHP</h4>
+          <div class="accordion" id="accordion2">
+            <?php include('step-1.html'); ?>
+            <?php include('step-2.html'); ?>
+            <?php include('step-3.html'); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <script src="public/js/jquery.min.js"></script>
+  <script src="public/js/tether.min.js"></script>
+  <script src="public/js/bootstrap.min.js"></script>
+</body>
+
+</html>
